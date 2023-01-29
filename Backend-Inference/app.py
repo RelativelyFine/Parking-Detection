2c71ad76a7dfaa599a0509282b697228ebe24040
David <53689532+RelativelyFine@users.noreply.github.com>
2023-01-29 07:43:04 -0500

    Crowd Support

--- Backend-Inference/app.py
+++ Backend-Inference/app.py
@@ -5,26 +5,12 @@ import os
 from threading import Lock
 
 model = YOLO('top-down-park.pt')  # load a pretrained YOLOv8n classification model
+
+model2 = YOLO('Crowd_Model.pt')
 lock = Lock()
 
 app = Flask(__name__)
 CORS(app)
-
-@app.route("/parking", methods=["POST"])
-@cross_origin()
-def process_image():
-    file = request.files['image']
-    lock.acquire()
-    # remove previous temp.png
-    if os.path.exists('temp.png'):
-        os.remove('temp.png')
-    # save image to disk
-    file.save('temp.png')
-    results = model.predict(source='temp.png', save=True)
-    last_dir = sorted(os.listdir('runs/detect'))[-1]
-    lock.release()
-
-    return send_file('runs/detect/' + last_dir + '/temp.png', mimetype='image/png')
 
 @app.route('/upload', methods = ['POST'])
 def upload_file():
@@ -36,6 +22,21 @@ def upload_file():
     # save image to disk
     file.save('temp.png')
     results = model.predict(source='temp.png', save=True)
+    last_dir = sorted(os.listdir('runs/detect'))[-1]
+    lock.release()
+    print("done!!!!!!!!!!!!!!!!!!!!!")
+    return send_file('runs/detect/' + last_dir + '/temp.png', mimetype='image/png')
+
+@app.route('/crowd', methods = ['POST'])
+def upload_file_crowd():
+    file = request.files['file']
+    lock.acquire()
+    # remove previous temp.png
+    if os.path.exists('temp.png'):
+        os.remove('temp.png')
+    # save image to disk
+    file.save('temp.png')
+    results = model2.predict(source='temp.png', save=True)
     last_dir = sorted(os.listdir('runs/detect'))[-1]
     lock.release()
     print("done!!!!!!!!!!!!!!!!!!!!!")
